b97a1fcf8861c3bf291b3c680c708922af154854
Neill <neillrobson811@gmail.com>
2015-09-07 21:31:20 -0400

    Redesign and style header with dynamic color
    Two headers are "rendered" on the screen, but one is invisible. Each of
    them are made to display on only one background color (dark splash color
    vs light body color). Sliding divs reveal one or the other depending on
    what is appropriate. Controlled by jQuery.

--- src/script/main.js
+++ src/script/main.js
@@ -1,6 +1,15 @@
 $(window).scroll(function() {
     var diff = $("#splash").height() - $(window).scrollTop();
-    if (diff <= $("body > header").outerHeight() && diff >= 0) {
-        console.log(diff);
+    var headerHeight = $(".headerdiv").outerHeight();
+    if (diff <= headerHeight && diff >= 0) {
+        var percentChange = 100 * (1 - diff / headerHeight);
+    } else if (diff < 0) {
+        var percentChange = 100;
+    } else {
+        var percentChange = 0;
     }
+    console.log(percentChange);
+    $(".headerdiv").css("transform", "translateY(-" + percentChange + "%" + ")");
+    $(".headerdivinner.dark").css("transform", "translateY(" + percentChange + "%" + ")");
+    $(".headerdivinner.light").css("transform", "translateY(" + (percentChange - 100) + "%" + ")");
 });
