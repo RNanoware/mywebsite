e21b2e413d7fdf63f2d24688dd8c499ceefd7689
Neill <neillrobson811@gmail.com>
2015-09-19 20:20:26 -0400

    Add smooth scrolling for intra-site links

--- src/script/main.js
+++ src/script/main.js
@@ -1,14 +1,27 @@
-$(window).scroll(function() {
-    var diff = $("#splash").height() - $(window).scrollTop();
-    var headerHeight = $(".headerdiv").outerHeight();
-    if (diff <= headerHeight && diff >= 0) {
-        var percentChange = 100 * (1 - diff / headerHeight);
-    } else if (diff < 0) {
-        var percentChange = 100;
-    } else {
-        var percentChange = 0;
-    }
-    $(".headerdiv").css("transform", "translateY(-" + percentChange + "%" + ")");
-    $(".headerdivinner.dark").css("transform", "translateY(" + percentChange + "%" + ")");
-    $(".headerdivinner.light").css("transform", "translateY(" + (percentChange - 100) + "%" + ")");
+// Run whenever document is ready
+$(function() {
+    // Smooth scrolling for any intra-document links (href="#id")
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (this.hash.length) {
+            $('body').animate({
+                scrollTop: $(this.hash).offset().top
+            }, 1000);
+        }
+    });
+
+    // Header shadow transition (between light and dark)
+    $(window).scroll(function() {
+        var diff = $("#splash").height() - $(window).scrollTop();
+        var headerHeight = $(".headerdiv").outerHeight();
+        if (diff <= headerHeight && diff >= 0) {
+            var percentChange = 100 * (1 - diff / headerHeight);
+        } else if (diff < 0) {
+            var percentChange = 100;
+        } else {
+            var percentChange = 0;
+        }
+        $(".headerdiv").css("transform", "translateY(-" + percentChange + "%" + ")");
+        $(".headerdivinner.dark").css("transform", "translateY(" + percentChange + "%" + ")");
+        $(".headerdivinner.light").css("transform", "translateY(" + (percentChange - 100) + "%" + ")");
+    });
 });
